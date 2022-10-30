f150b4a681a1db331c38c033f00d775d0d1c3e45
beegy <beegy.net@gmail.com>
2022-10-31 02:01:28 +0900

    init develop commit

---
+++ app/metadata/tags.py
@@ -0,0 +1,10 @@
+tags = [
+    {
+        "name": "author",
+        "description": "Contains CRUD operation on Authors",
+    },
+    {
+        "name": "book",
+        "description": "Contains CRUD operation on Books",
+    },
+]
